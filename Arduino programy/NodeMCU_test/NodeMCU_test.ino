9c066a0125d995255e228241344e72dc272f413b
JakubNowek <jakubbnowek@gmail.com>
2021-12-29 19:55:17 +0100

    NodeMcu

---
+++ Arduino programy/NodeMCU_test/NodeMCU_test.ino
@@ -0,0 +1,23 @@
+
+#include <LiquidCrystal_I2C.h>
+#include <Wire.h>
+LiquidCrystal_I2C lcd(0x27, 16, 2);
+
+void setup() {
+  Wire.begin();
+  lcd.begin(16,2);
+  lcd.init();
+
+  // Turn on the backlight.
+  lcd.backlight();
+
+  // Move the cursor characters to the right and
+  // zero characters down (line 1).
+  lcd.setCursor(0,0);
+
+    
+  lcd.print("W koncu dzialam");
+}
+
+void loop() {
+}
